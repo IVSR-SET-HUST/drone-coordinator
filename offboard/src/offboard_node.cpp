14bbcc68d5e8c300fbc58f266cb0c3f65c245cb0
congtranv <congtranv@outlook.com>
2020-10-06 10:55:10 +0700

    update offboard node, replace input quaternion with input roll pitch yaw

--- offboard/src/offboard_node.cpp
+++ offboard/src/offboard_node.cpp
@@ -32,6 +32,7 @@ int main(int argc, char **argv)
         ros::spinOnce();
         rate.sleep();
     }
+    ROS_INFO("FCU connected");
 
 	// check current state and position
 	for(int i = 10; ros::ok() && i > 0; --i)
@@ -57,7 +58,7 @@ int main(int argc, char **argv)
         rate.sleep();
     }
 
-	input_target();
+	input_local_target();
     target_pose.pose.position.x = target_pos[0][0];
     target_pose.pose.position.y = target_pos[0][1];
     target_pose.pose.position.z = target_pos[0][2];
