e00ba727fc6bdd4e7dcf0e5cb75fca95b5385640
congtranv <congtranv@outlook.com>
2020-10-09 10:30:21 +0700

    update hovering offboard

--- offboard/src/hovering_node.cpp
+++ offboard/src/hovering_node.cpp
@@ -4,26 +4,36 @@
 
 #include <iostream>
 
+// declare variants
 mavros_msgs::State current_state;
-void state_cb(const mavros_msgs::State::ConstPtr& msg){
+geometry_msgs::PoseStamped current_pose;
+geometry_msgs::PoseStamped target_pose;
+
+// callback functions
+void state_cb(const mavros_msgs::State::ConstPtr& msg)
+{
     current_state = *msg;
 }
 
-geometry_msgs::PoseStamped current_pose;
-void pose_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
+void pose_cb(const geometry_msgs::PoseStamped::ConstPtr& msg)
+{
     current_pose = *msg;
 }
 
+// main functions
 int main(int argc, char **argv)
 {
+    // initialize ros node
     ros::init(argc, argv, "hovering");
     ros::NodeHandle nh;
 
+    // subscriber
     ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
             ("mavros/state", 10, state_cb);
     ros::Subscriber local_pose_sub = nh.subscribe<geometry_msgs::PoseStamped>
             ("mavros/local_position/pose", 10, pose_cb);
 
+    // publisher
     ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
             ("mavros/setpoint_position/local", 10);
 
@@ -31,42 +41,44 @@ int main(int argc, char **argv)
     ros::Rate rate(20.0);
 
     // wait for FCU connection
-    while(ros::ok() && !current_state.connected){
+    while(ros::ok() && !current_state.connected)
+    {
+        ROS_INFO_ONCE("Connecting to FCU ...");
         ros::spinOnce();
         rate.sleep();
     }
+    ROS_INFO("FCU connected");
 
-	// check current state and position
+	// check current position
 	for(int i = 10; ros::ok() && i > 0; --i)
 	{
-		ROS_INFO_STREAM("\nCurrent state: \n" << current_state);
 		ROS_INFO_STREAM("\nCurrent pose: \n" << current_pose.pose.position);	
 		ros::spinOnce();
         rate.sleep();
     }
 
-    geometry_msgs::PoseStamped target_pose;
+    // input target position, hovering at current (x, y) 
     target_pose.pose.position.x = current_pose.pose.position.x;
     target_pose.pose.position.y = current_pose.pose.position.y;
-	std::cout << "Input hight target: " << std::endl;
-	// std::cout << "x: "; std::cin >> target_pose.pose.position.x;
-	// std::cout << "y: "; std::cin >> target_pose.pose.position.y;
+	std::cout << "Input a height for hovering: " << std::endl;
 	std::cout << "z: "; std::cin >> target_pose.pose.position.z;
 
     // send a few setpoints before starting
-    for(int i = 100; ros::ok() && i > 0; --i){
+    for(int i = 100; ros::ok() && i > 0; --i)
+    {
+        target_pose.header.stamp = ros::Time::now();
         local_pos_pub.publish(target_pose);
         ros::spinOnce();
         rate.sleep();
     }
+    ROS_INFO("Ready");
 
-    ros::Time last_request = ros::Time::now();
-
-    while(ros::ok()){
-		ROS_INFO_STREAM("\nCurrent state: \n" << current_state);
+    // publish target, keep drone hovering
+    while(ros::ok())
+    {
         ROS_INFO_STREAM("\nCurrent position: \n" << current_pose.pose.position);	
-	    // ROS_INFO_STREAM("\nTarget position: \n" << target_pose.pose.position);
 
+        target_pose.header.stamp = ros::Time::now();   
         local_pos_pub.publish(target_pose);
         ros::spinOnce();
         rate.sleep();
